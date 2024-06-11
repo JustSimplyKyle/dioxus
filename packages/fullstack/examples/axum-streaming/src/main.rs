bd76eae7f7bc53dcacae5345adff2197dc015d2c
Evan Almloff <evanalmloff@gmail.com>
2024-06-11 07:13:01 -0500

    use typed streaming server functions in the axum streaming example

--- packages/fullstack/examples/axum-streaming/src/main.rs
+++ packages/fullstack/examples/axum-streaming/src/main.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 use futures::StreamExt;
-use server_fn::codec::{StreamingText, TextStream};
+use serde::{Deserialize, Serialize};
+use server_fn::codec::{JsonStream, StreamingJson};
 
 fn app() -> Element {
     let mut response = use_signal(String::new);
@@ -10,10 +11,14 @@ fn app() -> Element {
             onclick: move |_| async move {
                 response.write().clear();
                 if let Ok(stream) = test_stream().await {
+                    tracing::info!("Stream started");
                     response.write().push_str("Stream started\n");
                     let mut stream = stream.into_inner();
                     while let Some(Ok(text)) = stream.next().await {
-                        response.write().push_str(&text);
+                        tracing::info!("{text:?}");
+                        let num = text.number * text.squared;
+                        let text = text.text;
+                        response.write().push_str(&format!("{num}: {text}\n"));
                     }
                 }
             },
@@ -23,19 +28,36 @@ fn app() -> Element {
     }
 }
 
-#[server(output = StreamingText)]
-pub async fn test_stream() -> Result<TextStream, ServerFnError> {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    number: u64,
+    squared: u64,
+    text: String,
+}
+
+#[server(output = StreamingJson)]
+pub async fn test_stream() -> Result<JsonStream<Message>, ServerFnError> {
     let (tx, rx) = futures::channel::mpsc::unbounded();
     tokio::spawn(async move {
+        let mut number = 0;
         loop {
             tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-            let _ = tx.unbounded_send(Ok("Hello, world!".to_string()));
+            println!("Sending message {}", number);
+            let _ = tx.unbounded_send(Ok(Message {
+                number,
+                squared: number * number,
+                text: "Hello, world!".to_string(),
+            }));
+            number += 1;
         }
     });
 
-    Ok(TextStream::new(rx))
+    Ok(JsonStream::new(rx))
 }
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    tracing_wasm::set_as_global_default();
+
     launch(app)
 }
